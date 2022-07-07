de25a32ae70736a66fa0f74adaa547116dd7211e
Jhenil Parihar <jhenilparihar@gmail.com>
2022-07-07 16:29:59 +0530

    define components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
+import { Navbar, Welcome, Footer, Services, Transaction } from "./components";
 const App = () => {
-  
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transaction />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
