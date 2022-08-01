e425ae9b126ae1ee8c5f70e5febfe410df276c3f
Jhenil Parihar <jhenilparihar@gmail.com>
2022-08-01 12:30:37 +0530

    goerli network url defined

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/cZvGmFGIQLUOhhhhQfrG_-QiQIa9rJmv
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
-  solidity: "0.8.9",
-};
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/cZvGmFGIQLUOhhhhQfrG_-QiQIa9rJmv',
+      accounts: ['2b5090b74867e753f289fea1a8b492d7989f392dfbb702d8d7d918f5b9530ecb']
+    }
+  }
+}
