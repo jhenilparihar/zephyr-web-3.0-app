a33b4b42b3a9f2353bcab1d93e24c09f2124ac08
Jhenil Parihar <jhenilparihar@gmail.com>
2022-08-02 13:19:42 +0530

    contract deployed to new address

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,6 @@
 import abi from './Transactions.json';
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x48fE98916927026182a7Bb9dF86783c37D91E1BB';
+export const contractAddress = '0x757A323dB70EF933159687030207DbBd10FddC27';
+
+// 0x757A323dB70EF933159687030207DbBd10FddC27
